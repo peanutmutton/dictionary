089d723e7c3792f5724eedbd44516118665b281a
peanutmutton <peanutmutton@outlook.com>
2024-10-29 17:46:16 +0500

    GET POST, tests

---
+++ src/main/java/sia/dictionary/model/DictionaryRepository.java
@@ -0,0 +1,5 @@
+package sia.dictionary.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface DictionaryRepository extends CrudRepository<DictionaryEntry, Integer> {}
